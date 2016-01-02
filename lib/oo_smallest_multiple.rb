12947c1a7f42de2c9cf52243821ac98078308e29
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-02 22:57:19 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,34 @@
 # Implement your object-oriented solution here!
+
+class SmallestMultiple
+  def initialize(max)
+    @max = max
+  end
+  
+  def lowest_factor_of(num)
+    low_primes = [2, 3, 5, 7, 11, 13, 17, 19]
+    lowest_multiple = 1
+    low_primes.each do |prime| 
+      if num % prime == 0 
+        lowest_multiple = prime
+        break
+      end
+    end
+    
+    lowest_multiple
+  end
+  
+  def lcm
+    factors = (1..@max).to_a
+    mm = 1
+    
+    factors.each_with_index do |f, i|
+      next_f = factors[i + 1]
+      unless next_f == nil
+        mm *= lowest_factor_of(next_f) unless mm % next_f == 0
+      end
+    end
+    
+    mm
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,34 @@
 # Implement your procedural solution here!
+require 'pry'
+require 'pry-nav'
+
+def lowest_factor_of(num)
+  low_primes = [2, 3, 5, 7, 11, 13, 17, 19]
+  lowest_multiple = 1
+  low_primes.each do |prime| 
+    if num % prime == 0 
+      lowest_multiple = prime
+      break
+    end
+  end
+  
+  lowest_multiple
+end
+
+
+def smallest_multiple(max)
+  factors = (1..max).to_a
+  min_multiple = 1
+  
+  factors.each_with_index do |f, i|
+    next_f = factors[i + 1]
+    unless next_f == nil
+      unless min_multiple % next_f == 0
+        min_multiple *= lowest_factor_of(next_f)
+      end
+    end
+  end
+  
+  min_multiple
+end
+
